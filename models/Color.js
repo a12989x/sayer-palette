1004ffa72b81821329eaf9358d42aa1a9b71b844
a12989x <alexis12989x@gmail.com>
2020-07-15 15:41:52 -0500

    Create basic mongoose model

---
+++ models/Color.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ColorSchema = new Schema({
+    codeNumber: { type: String, required: true },
+    colorName: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Color', ColorSchema);
