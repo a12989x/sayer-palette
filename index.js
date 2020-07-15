0a1be6e5fff5904de5ee640a969d9768af6e7588
a12989x <alexis12989x@gmail.com>
2020-07-15 15:39:24 -0500

    Use colorsRoute
    Import colorsRoute and use for get a hello world

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const colorsRoute = require('./routes/colors');
+
 const app = express();
 dotenv.config();
 
@@ -11,6 +13,8 @@ mongoose.connect(
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => console.log('Connected to MongoDB')
 );
+
+app.use('/', colorsRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
