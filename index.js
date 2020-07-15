0fac229ba289980fd0a0a05ec0d3698c34912400
a12989x <alexis12989x@gmail.com>
2020-07-15 15:32:36 -0500

    Connect to MongoDB
    Import mongoose to index.js, connect to MongoDB and config

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 const app = express();
 dotenv.config();
 
 app.use(express.json());
+mongoose.connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('Connected to MongoDB')
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
