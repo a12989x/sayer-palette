60bc1b26a784b94c4aa5c265432b4d04637f0c74
a12989x <alexis12989x@gmail.com>
2020-07-16 11:07:47 -0500

    Create Color Schema

--- models/Color.js
+++ models/Color.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ColorSchema = new Schema({
-    codeNumber: { type: String, required: true },
-    colorName: { type: String, required: true },
-});
+const ColorSchema = new Schema(
+    {
+        codeNumber: { type: String, required: true },
+        colorName: { type: String, required: true },
+        hexCode: { type: String, required: true },
+        base: [],
+    },
+    { validateBeforeSave: true }
+);
 
 module.exports = mongoose.model('Color', ColorSchema);
