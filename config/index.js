e969e192598c7abcd02097e7853ecfce325a7a6b
a12989x <alexis12989x@gmail.com>
2020-07-18 13:40:20 -0500

    Create configuration for enviroment variables

---
+++ config/index.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT | 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+const SECRET = process.env.SECRET;
+
+module.exports = { PORT, MONGODB_URI, SECRET };
--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const { success, error } = require('consola');
 
+const { PORT, MONGODB_URI } = require('./config');
+
 const colorsRoute = require('./routes/colors');
 
 const app = express();
-dotenv.config();
 
 app.use(express.json());
 mongoose.connect(
-    process.env.MONGODB_URI,
+    MONGODB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => console.log('Connected to MongoDB')
 );
 
 app.use('/api', colorsRoute);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
