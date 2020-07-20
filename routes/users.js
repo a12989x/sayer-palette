e71edeafe018ddd5f6235f5d7b19d4b8e3d2084e
a12989x <alexis12989x@gmail.com>
2020-07-20 17:25:23 -0500

    Add middleware to register normal user

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const { userRegister } = require('../utils/Auth.js');
+const userValidationMiddleware = require('../middlewares/validateUser');
+const schemas = require('../middlewares/userSchema');
 
-router.post('/register-user', async (req, res) => {
-    await userRegister(req.body, 'user', res);
-});
+router.post(
+    '/register-user',
+    userValidationMiddleware(schemas.registerSchema),
+    async (req, res) => {
+        await userRegister(req.body, 'user', res);
+    }
+);
 
 module.exports = router;
