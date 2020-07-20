f8d474e69afec5d8e3cbf22407ff147d67e594ca
a12989x <alexis12989x@gmail.com>
2020-07-20 17:33:07 -0500

    Create middleware for check credentials to register

--- middlewares/validateUser.js
+++ middlewares/validateUser.js
@@ -1,26 +1,33 @@
 const Joi = require('joi');
 
-const loginSchema = Joi.object({
-    email: Joi.string().email().min(4).max(100).required(),
-    username: Joi.string().min(5).max(50),
-    password: Joi.string().min(8).max(50),
-});
+const schemas = {
+    loginSchema: Joi.object().keys({
+        email: Joi.string().email().min(4).max(100).required(),
+        username: Joi.string().min(5).max(50),
+        password: Joi.string().min(8).max(50),
+    }),
 
-const registerSchema = Joi.object({
-    name: Joi.string().min(3).max(80).required(),
-    // email: Joi.string()
-    //     .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-    //     .min(4)
-    //     .max(100)
-    //     .required(),
-    // username: Joi.string().min(5).max(50),
-    // password: Joi.string().min(8).max(150).required(),
-    // repeat_password: Joi.ref('password'),
-});
+    registerSchema: Joi.object().keys({
+        name: Joi.string().min(3).max(80).required(),
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+            .min(4)
+            .max(100)
+            .required(),
+        username: Joi.string().min(5).max(50),
+        password: Joi.string().min(8).max(150).required(),
+        repeat_password: Joi.ref('password'),
+    }),
+};
 
-const loginValidation = (data) => loginSchema.validateAsync(data, loginSchema);
+const userValidationMiddleware = (schema) => (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+        const { details } = error;
+        const message = details.map((i) => i.message).join(',');
+        res.status(422).json({ error: message, success: false });
+    }
+    next();
+};
 
-const registerValidation = async (data) =>
-    await registerSchema.validate(data, registerSchema);
-
-module.exports = { loginValidation, registerValidation, registerSchema };
+module.exports = { userValidationMiddleware, schemas };
