3452307faaa52ab27db25aeec3fb156433eaaca1
a12989x <alexis12989x@gmail.com>
2020-07-20 13:18:57 -0500

    Add useFindAndModify to mongoose config for connection

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ const { success, error } = require('consola');
 const { PORT, MONGODB_URI } = require('./config');
 
 const colorsRoute = require('./routes/colors');
+const usersRoute = require('./routes/users');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api', colorsRoute);
+app.use('/api/users', usersRoute);
 
 const startApp = async () => {
     try {
         await connect(MONGODB_URI, {
             useNewUrlParser: true,
+            useFindAndModify: true,
             useUnifiedTopology: true,
         });
         success({ message: 'Connected to MongoDB', badge: true });
