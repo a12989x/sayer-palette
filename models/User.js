a3a81f3182d23c13d2e96270f6b6e2ecc3f8b103
a12989x <alexis12989x@gmail.com>
2020-07-21 17:16:31 -0500

    Add email validation with regex

--- models/User.js
+++ models/User.js
@@ -3,7 +3,13 @@ const { Schema, model } = require('mongoose');
 const UserSchema = new Schema(
     {
         name: { type: String, required: true, min: 3, max: 80 },
-        email: { type: String, required: true, min: 4, max: 100 },
+        email: {
+            type: String,
+            required: true,
+            min: 4,
+            max: 100,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
+        },
         username: { type: String, required: true, min: 5, max: 50 },
         password: { type: String, required: true, min: 8, max: 250 },
         role: {
