e6bc1a863245574ab376cc1058252aacf5c1d303
a12989x <alexis12989x@gmail.com>
2020-07-22 17:53:29 -0500

    Change fucntions to another file

--- routes/colors.js
+++ routes/colors.js
@@ -1,95 +1,38 @@
 const express = require('express');
-const Color = require('../models/Color');
-
 const router = express.Router();
 
 const { userAuth, checkRole, serializeUser } = require('../middlewares/auth');
+const {
+    getColors,
+    createColor,
+    getColor,
+    updateColor,
+    deleteColor,
+} = require('../controllers/color');
 
 router.get(
     '/',
     userAuth,
     checkRole(['user', 'admin', 'superadmin']),
-    async (req, res) => {
-        try {
-            const allColors = await Color.find();
-            res.json(allColors);
-        } catch (error) {
-            res.status(500).json({ msg: error });
-        }
-    }
+    getColors
 );
 
-router.post(
-    '/new',
+router.post('/new', userAuth, checkRole(['admin', 'superadmin']), createColor);
+
+router.get(
+    '/:id',
+    userAuth,
+    checkRole(['user', 'admin', 'superadmin']),
+    getColor
+);
+
+router.patch('/:id', userAuth, checkRole(['admin', 'superadmin']), updateColor);
+
+router.delete(
+    '/:id',
     userAuth,
     checkRole(['admin', 'superadmin']),
-    async (req, res) => {
-        try {
-            const { codeNumber, colorName, base, hexCode } = req.body;
-
-            const newColor = await new Color({
-                codeNumber,
-                colorName,
-                base,
-                hexCode,
-            });
-
-            await newColor.save();
-            res.status(201).json({
-                message: 'New color created.',
-                success: true,
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: 'Unable to create your color.',
-                success: false,
-            });
-        }
-    }
+    deleteColor
 );
-
-router.get('/:id', async (req, res) => {
-    const color = await Color.findById(req.params.id);
-
-    res.send(color);
-});
-
-router.patch('/:id', async (req, res) => {
-    try {
-        await Color.updateOne(
-            { _id: req.params.id },
-            {
-                $set: {
-                    codeNumber: req.body.codeNumber,
-                    colorName: req.body.colorName,
-                    hexCode: req.body.hexCode,
-                    base: req.body.base,
-                },
-            }
-        );
-
-        res.status(201).json({
-            message: 'Edited successfully.',
-            success: true,
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Unable to create your new color.',
-            success: false,
-        });
-    }
-});
-
-router.delete('/:id', async (req, res) => {
-    try {
-        const colorRemove = await Color.remove({ _id: req.params.id });
-        res.json({ message: 'Color removed successfully.', success: true });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Unable to delete the color.',
-            success: false,
-        });
-    }
-});
 
 module.exports = router;
