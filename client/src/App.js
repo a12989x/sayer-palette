9189cf5a88ffa6caa255d7af78b49b1f5fbc36ae
a12989x <alexis12989x@gmail.com>
2020-07-24 12:32:45 -0500

    Create Route for exact root path eith Home Component
    Import History and Home Component for Create a router with root path

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
 
+import History from './components/History';
+import Home from './components/pages/Home';
+
 const App = () => {
     return (
         <div className="App">
-            <p>App Component</p>
+            <Router history={History}>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                </Switch>
+            </Router>
         </div>
     );
 };
