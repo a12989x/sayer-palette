cce3deac5d207556a95587da98a4d81cb7f6372e
a12989x <alexis12989x@gmail.com>
2020-07-24 16:22:27 -0500

    Create form for registration

--- client/src/components/access/Register.js
+++ client/src/components/access/Register.js
@@ -3,7 +3,25 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            <p>Register Component</p>
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" />
+
+            <label htmlFor="secondName">Second Name</label>
+            <input type="text" id="secondName" />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input type="password" id="confirmPassword" />
+
+            <button type="submit">Register</button>
         </div>
     );
 };
