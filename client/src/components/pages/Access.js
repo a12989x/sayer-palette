5e41bf22f2fb0662a5cb79b6c5af195f4137bd24
a12989x <alexis12989x@gmail.com>
2020-07-24 13:02:29 -0500

    Import SignIn and Register component and render

--- client/src/components/pages/Access.js
+++ client/src/components/pages/Access.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import SignIn from '../access/SignIn';
+import Register from '../access/Register';
+
 const Access = () => {
     return (
         <div>
             <p>Access Component</p>
+            <SignIn />
+            <Register />
         </div>
     );
 };
