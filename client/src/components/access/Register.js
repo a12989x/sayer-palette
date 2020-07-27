6a6148bef3dcbee2690ea563a7f1aa9ac93cf3b5
a12989x <alexis12989x@gmail.com>
2020-07-27 16:48:44 -0500

    Create Validation and add info for form

--- client/src/components/access/Register.js
+++ client/src/components/access/Register.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
+import Tippy from '@tippyjs/react';
+
+import 'tippy.js/dist/tippy.css';
+
+import { useForm } from '../useForm';
 
 const Register = () => {
-    const [emailValidation, setEmailValidation] = useState(true);
+    const [isEmailValidate, setIsEmailValidate] = useState(true);
+    const [isPasswordValidate, setIsPasswordValidate] = useState(true);
+    const [isConfirmPasswordValidate, setIsConfirmPasswordValidate] = useState(
+        true
+    );
+
+    const [values, handleChange] = useForm({
+        firstName: '',
+        lastName: '',
+        email: '',
+        username: '',
+        password: '',
+        confirmPassword: '',
+    });
 
     const validateEmail = (e) => {
         const email = e.target.value;
@@ -11,11 +29,30 @@ const Register = () => {
         };
 
         if (email.length === 0) {
-            setEmailValidation(true);
+            setIsEmailValidate(true);
             return;
         }
-        if (validate(email)) console.log('true');
-        else setEmailValidation(false);
+        if (validate(email)) setIsEmailValidate(true);
+        else setIsEmailValidate(false);
+    };
+
+    const validatePassword = (e) => {
+        const password = e.target.value;
+
+        if (password.length === 0) setIsPasswordValidate(true);
+        else if (password.length < 3 || password.length > 50)
+            setIsPasswordValidate(false);
+        else setIsPasswordValidate(true);
+    };
+
+    const validateConfirmPassword = (e) => {
+        const confirmPassword = e.target.value;
+
+        if (confirmPassword.length === 0) setIsConfirmPasswordValidate(true);
+        else if (confirmPassword !== values.password)
+            setIsConfirmPasswordValidate(false);
+        else if (confirmPassword === values.password)
+            setIsConfirmPasswordValidate(true);
     };
 
     return (
@@ -23,25 +60,118 @@ const Register = () => {
             <label htmlFor="firstName">First Name</label>
             <label htmlFor="secondName">Second Name</label>
 
-            <input type="text" id="firstName" />
-            <input type="text" id="secondName" />
+            <input
+                required={true}
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={values.firstName}
+                onChange={handleChange}
+            />
+            <input
+                required={true}
+                type="text"
+                id="secondName"
+                name="lastName"
+                value={values.lastName}
+                onChange={handleChange}
+            />
 
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">
+                Email
+                <Tippy
+                    placement="right"
+                    content={<span>Please Enter a valid email</span>}
+                >
+                    <span
+                        className={`tooltip tooltip-email${
+                            !isEmailValidate ? '-active' : ''
+                        }`}
+                    >
+                        i
+                    </span>
+                </Tippy>
+            </label>
             <label htmlFor="username">Username</label>
 
-            <input type="email" id="email" onChange={(e) => validateEmail(e)} />
-            {!emailValidation && (
-                <span className="register__email-validation-text">
-                    Please enter a valid email
-                </span>
-            )}
-            <input type="text" id="username" />
+            <input
+                required={true}
+                type="email"
+                id="email"
+                onChange={(e) => {
+                    validateEmail(e);
+                    handleChange(e);
+                }}
+            />
 
-            <label htmlFor="password">Password</label>
-            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+                required={true}
+                type="text"
+                id="username"
+                name="username"
+                value={values.username}
+                onChange={handleChange}
+            />
 
-            <input type="password" id="password" />
-            <input type="password" id="confirmPassword" />
+            <label htmlFor="password">
+                Password
+                <Tippy
+                    placement="right"
+                    content={<span>8 characters minimum and 50 maximum</span>}
+                >
+                    <span
+                        className={`tooltip tooltip-password${
+                            !isPasswordValidate ? '-active' : ''
+                        }`}
+                    >
+                        i
+                    </span>
+                </Tippy>{' '}
+            </label>
+            <label htmlFor="confirmPassword">
+                Confirm Password
+                <Tippy
+                    placement="right"
+                    content={
+                        <span>
+                            {values.password.length === 0
+                                ? 'Please enter a password first'
+                                : 'Passwords must match'}
+                        </span>
+                    }
+                >
+                    <span
+                        className={`tooltip tooltip-confirm-password${
+                            !isConfirmPasswordValidate ? '-active' : ''
+                        }`}
+                    >
+                        i
+                    </span>
+                </Tippy>
+            </label>
+
+            <input
+                required={true}
+                type="password"
+                id="password"
+                name="password"
+                value={values.password}
+                onChange={(e) => {
+                    validatePassword(e);
+                    handleChange(e);
+                }}
+            />
+            <input
+                required={true}
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                value={values.confirmPassword}
+                onChange={(e) => {
+                    validateConfirmPassword(e);
+                    handleChange(e);
+                }}
+            />
 
             <button type="submit">Register</button>
         </form>
