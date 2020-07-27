f428e038fa4979b8cfdaba9bee8b52a0f72d7702
a12989x <alexis12989x@gmail.com>
2020-07-27 17:39:07 -0500

    Create function for return data on submit form

--- client/src/components/access/SignIn.js
+++ client/src/components/access/SignIn.js
@@ -6,8 +6,21 @@ import { useForm } from '../useForm';
 const SignIn = () => {
     const [values, handleChange] = useForm({ username: '', password: '' });
 
+    const signIn = async (e) => {
+        e.preventDefault();
+        const { username, password } = values;
+
+        try {
+            const params = { username, password };
+            const res = await axios.post('/api/users/login-user', params);
+            console.log(res.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
-        <form className="sign-in">
+        <form className="sign-in" onSubmit={(e) => signIn(e)}>
             <label htmlFor="username">Username</label>
             <input
                 required={true}
