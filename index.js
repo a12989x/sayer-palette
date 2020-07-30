06c9d9996200b127f120d15461bdafa053252a40
a12989x <alexis12989x@gmail.com>
2020-07-30 13:09:23 -0500

    Add middewares for cookie-parser and wxpress-jwt

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { connect } = require('mongoose');
-const dotenv = require('dotenv');
 const passport = require('passport');
 const { success, error } = require('consola');
+const jwt = require('express-jwt');
+const cookieParser = require('cookie-parser');
 
-const { PORT, MONGODB_URI } = require('./config');
+const { PORT, MONGODB_URI, SECRET } = require('./config');
 
 const colorsRoute = require('./routes/colors');
 const usersRoute = require('./routes/users');
@@ -13,11 +14,20 @@ const app = express();
 
 app.use(express.json());
 app.use(passport.initialize());
+app.use(cookieParser());
 
 require('./middlewares/passport')(passport);
 
 app.use('/api/colors', colorsRoute);
 app.use('/api/users', usersRoute);
+
+app.use(
+    jwt({
+        secret: SECRET,
+        algorithms: ['HS256'],
+        getToken: (req) => req.cookies.token,
+    })
+);
 
 const startApp = async () => {
     try {
