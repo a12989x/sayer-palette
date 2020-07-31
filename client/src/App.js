590c6b500b71b7d4bbf7cbf9f4f142a85a92f6eb
a12989x <alexis12989x@gmail.com>
2020-07-31 12:22:23 -0500

    Import AuthContextProviders

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,19 @@ import History from './components/History';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import Access from './components/pages/Access';
+import AuthContextProvider from './contexts/AuthContext';
 
 const App = () => {
     return (
         <div className="App">
             <Router history={History}>
-                <Navbar />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/access" component={Access} />
-                </Switch>
+                <AuthContextProvider>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/access" component={Access} />
+                    </Switch>
+                </AuthContextProvider>
             </Router>
         </div>
     );
