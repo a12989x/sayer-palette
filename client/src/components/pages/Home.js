c49118bee4383397eeec78ccbd998b894c8a7a95
a12989x <alexis12989x@gmail.com>
2020-07-31 17:09:00 -0500

    Get colors from dabase

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import History from '../History';
 
 const Home = () => {
+    const [colors, setColors] = useState({});
+
+    useEffect(() => {
+        getColors();
+    }, []);
+
+    const getColors = async () => {
+        try {
+            const res = await axios.get('/api/colors');
+            console.log(res.data);
+        } catch (error) {
+            console.error('You need sign in');
+            History.push('/access');
+        }
+    };
+
     return (
         <div>
             <p>Home Component</p>
