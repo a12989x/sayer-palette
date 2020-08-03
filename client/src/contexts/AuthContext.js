ba5f421c21def0af086bf56f08e95e7bfa01b3c6
a12989x <alexis12989x@gmail.com>
2020-08-03 09:42:18 -0500

    Create function for SignIn

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,12 +1,38 @@
 import React, { useState, createContext } from 'react';
+import axios from 'axios';
+
+import History from '../components/History';
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = (props) => {
     const [isSignIn, setIsSignIn] = useState(false);
+    const [user, setUser] = useState({});
+
+    const signIn = async (e, values) => {
+        e.preventDefault();
+        const { username, password } = values;
+
+        try {
+            const params = { username, password };
+            const res = await axios.post('/api/users/login-user', params);
+            History.push('/');
+            console.log(res.data);
+            setIsSignIn(true);
+            setUser({
+                username: res.data.username,
+                email: res.data.email,
+                role: res.data.role,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const register = () => {};
 
     return (
-        <AuthContext.Provider value={{ isSignIn, setIsSignIn }}>
+        <AuthContext.Provider value={{ isSignIn, setIsSignIn, signIn }}>
             {props.children}
         </AuthContext.Provider>
     );
