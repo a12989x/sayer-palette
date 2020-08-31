edbf7fae15d562b438213cc25ead084a2157a30a
a12989x <alexis12989x@gmail.com>
2020-08-31 09:46:20 -0500

    Add props values

--- client/src/components/new-color/Size.js
+++ client/src/components/new-color/Size.js
@@ -3,14 +3,15 @@ import React, { useContext } from 'react';
 import { NewColorContext } from '../../contexts/NewColorContext';
 import Tint from './Tint';
 
-const Size = ({ name }) => {
+const Size = ({ name, values }) => {
     const { sizes } = useContext(NewColorContext);
+    const { firstBaseLiterFirstTint } = values;
 
     return (
         <section className="size">
             <h4>{name}</h4>
 
-            <Tint />
+            <Tint values={firstBaseLiterFirstTint} />
             <Tint />
             <Tint />
             <Tint />
