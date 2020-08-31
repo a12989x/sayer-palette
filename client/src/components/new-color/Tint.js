6d9d5e14f8b3eb5c7ae41e798b0cf9fbff5e6026
a12989x <alexis12989x@gmail.com>
2020-08-31 09:43:09 -0500

    Add conditional for disable inputs

--- client/src/components/new-color/Tint.js
+++ client/src/components/new-color/Tint.js
@@ -1,14 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 
 import { NewColorContext } from '../../contexts/NewColorContext';
 
-const NewTint = () => {
+const NewTint = ({ values, handleChange }) => {
     const { tints } = useContext(NewColorContext);
+    const [isSelect, setIsSelect] = useState(false);
 
     return (
         <section className="tint">
             <div className="select-wrapper">
-                <select name="tint" id="tint" defaultValue={tints[0]}>
+                <select
+                    name="tint"
+                    id="tint"
+                    defaultValue={tints[0]}
+                    onChange={(e) => {
+                        handleChange(e);
+                        values.tint !== tints[0]
+                            ? setIsSelect(true)
+                            : setIsSelect(false);
+                    }}
+                >
                     {tints.map((name, key) => (
                         <option
                             key={key}
@@ -22,13 +33,29 @@ const NewTint = () => {
             </div>
 
             <div className="input-wrapper">
-                <input type="text" name="oz" id="oz" required={true} />
+                <input
+                    type="text"
+                    name="oz"
+                    id="oz"
+                    required={true}
+                    value={values.first}
+                    onChange={handleChange}
+                    disabled={!isSelect}
+                />
                 <label htmlFor="oz">
                     <span>Oz.</span>
                 </label>
             </div>
             <div className="input-wrapper">
-                <input type="text" name="sixty" id="sixty" required={true} />
+                <input
+                    type="text"
+                    name="sixty"
+                    id="sixty"
+                    required={true}
+                    value={values.second}
+                    onChange={handleChange}
+                    disabled={!isSelect}
+                />
                 <label htmlFor="sixty">
                     <span>1 / 64</span>
                 </label>
@@ -39,6 +66,9 @@ const NewTint = () => {
                     name="oneHundred"
                     id="oneHundred"
                     required={true}
+                    value={values.third}
+                    onChange={handleChange}
+                    disabled={!isSelect}
                 />
                 <label htmlFor="oneHundred">
                     <span>1 / 128</span>
