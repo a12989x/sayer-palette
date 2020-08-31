d4dea623b721d9c1252d116b0dbd1a9c0c8faf38
a12989x <alexis12989x@gmail.com>
2020-08-31 09:44:53 -0500

    Add onChange fuction from context

--- client/src/components/new-color/Base.js
+++ client/src/components/new-color/Base.js
@@ -2,33 +2,28 @@ import React, { useState, useContext } from 'react';
 
 import { NewColorContext } from '../../contexts/NewColorContext';
 
-import Size from './Size';
-
-const Base = ({ name }) => {
+const Base = ({ value, handleChange }) => {
     const { bases } = useContext(NewColorContext);
 
     return (
-        <section className="base">
-            <h3>{name}</h3>
-
-            <div className="select-wrapper">
-                <select name="bases" id="bases" defaultValue={bases[0]}>
-                    {bases.map((name, key) => (
-                        <option
-                            key={key}
-                            value={name}
-                            disabled={name === bases[0] ? true : false}
-                        >
-                            {name}
-                        </option>
-                    ))}
-                </select>
-            </div>
-
-            <Size name="Liter" />
-            <Size name="Galon" />
-            <Size name="Bucket" />
-        </section>
+        <div className="select-wrapper">
+            <select
+                name="bases"
+                id="bases"
+                defaultValue={bases[0]}
+                onChange={handleChange}
+            >
+                {bases.map((name, key) => (
+                    <option
+                        key={key}
+                        value={name}
+                        disabled={name === bases[0] ? true : false}
+                    >
+                        {name}
+                    </option>
+                ))}
+            </select>
+        </div>
     );
 };
 
