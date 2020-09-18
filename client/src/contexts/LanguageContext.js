ee96dea97d0f1d27cac23f2b2ea08c1ab58e2f20
a12989x <alexis12989x@gmail.com>
2020-09-18 16:22:50 -0500

    Load default language from local storage

--- client/src/contexts/LanguageContext.js
+++ client/src/contexts/LanguageContext.js
@@ -1,11 +1,17 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import ls from 'local-storage';
 
 export const LanguageContext = createContext();
 
 const LanguageContextProvider = (props) => {
     const [language, setLanguage] = useState('en');
     const [t, i18n] = useTranslation();
+
+    useEffect(() => {
+        const lng = ls.get('i18nextLng');
+        setLanguage(lng);
+    }, []);
 
     const changeLanguage = () => {
         setLanguage(language === 'en' ? 'es' : 'en');
