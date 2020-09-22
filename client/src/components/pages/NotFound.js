1741e9fe0bc19702753acacb1909f09b7b3073aa
a12989x <alexis12989x@gmail.com>
2020-09-22 12:27:49 -0500

    Change redirect if users is sign in or not

--- client/src/components/pages/NotFound.js
+++ client/src/components/pages/NotFound.js
@@ -2,9 +2,11 @@ import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { LanguageContext } from '../../contexts/LanguageContext';
+import { AuthContext } from '../../contexts/AuthContext';
 
 const NoMatch = () => {
     const { t } = useContext(LanguageContext);
+    const { isSignIn } = useContext(AuthContext);
 
     return (
         <main className="not-found">
@@ -12,7 +14,7 @@ const NoMatch = () => {
                 <p>Oops!</p>
                 <h1>404 - {t('notFound.heading')}</h1>
                 <h3>{t('notFound.description')}</h3>
-                <NavLink to="/sign-in">
+                <NavLink to={!isSignIn ? '/sign-in' : '/get-color'}>
                     <button className="primary-button">
                         {t('notFound.button')}
                     </button>
