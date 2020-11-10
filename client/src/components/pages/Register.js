0a0aded9df32732fbb2526a580711335cad60c5e
a12989x <alexis12989x@gmail.com>
2020-11-10 16:58:53 -0600

    Delete validate function and add Tooltips

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -1,5 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { AiFillInfoCircle } from 'react-icons/ai';
+import { UncontrolledTooltip } from 'reactstrap';
 
 import 'tippy.js/dist/tippy.css';
 
@@ -8,8 +10,6 @@ import { AuthContext } from '../../contexts/AuthContext';
 import { LanguageContext } from '../../contexts/LanguageContext';
 
 const Register = () => {
-    const [isEmailValidate, setIsEmailValidate] = useState(true);
-    const [isPasswordValidate, setIsPasswordValidate] = useState(true);
     const [isConfirmPasswordValidate, setIsConfirmPasswordValidate] = useState(
         true
     );
@@ -25,30 +25,6 @@ const Register = () => {
         confirmPassword: '',
     });
 
-    const validateEmail = (e) => {
-        const email = e.target.value;
-        const validate = () => {
-            const re = /\S+@\S+\.\S+/;
-            return re.test(email);
-        };
-
-        if (email.length === 0) {
-            setIsEmailValidate(true);
-            return;
-        }
-        if (validate(email)) setIsEmailValidate(true);
-        else setIsEmailValidate(false);
-    };
-
-    const validatePassword = (e) => {
-        const password = e.target.value;
-
-        if (password.length === 0) setIsPasswordValidate(true);
-        else if (password.length < 8 || password.length > 50)
-            setIsPasswordValidate(false);
-        else setIsPasswordValidate(true);
-    };
-
     const validateConfirmPassword = (e) => {
         const confirmPassword = e.target.value;
 
@@ -60,97 +36,159 @@ const Register = () => {
     };
 
     return (
-        <form className="register" onSubmit={(e) => register(e, values)}>
-            <div className="container">
-                <h1 className="title">{t('register.title')}</h1>
-                <h4 className="description">{t('register.description')}</h4>
-                <div className="input-wrapper">
+        <form className='register' onSubmit={(e) => register(e, values)}>
+            <div className='container'>
+                <h1 className='title'>{t('register.title')}</h1>
+                <h4 className='description'>{t('register.description')}</h4>
+                <div className='input-wrapper'>
                     <input
                         required={true}
-                        type="text"
-                        id="name"
-                        name="name"
+                        type='text'
+                        id='name'
+                        name='name'
                         value={values.firstName}
                         autoFocus={true}
+                        minLength='3'
+                        maxLength='80'
                         onChange={handleChange}
                     />
-                    <label htmlFor="name">
+                    <label htmlFor='name'>
                         <span>{t('register.name')}</span>
                     </label>
+                    <AiFillInfoCircle id='nameFillInfoCircle' />
+                    <UncontrolledTooltip
+                        placement='left'
+                        target='nameFillInfoCircle'
+                        className='tooltip'
+                    >
+                        <ul>
+                            <li>min: 3</li> <li>max: 80</li>
+                        </ul>
+                    </UncontrolledTooltip>
                 </div>
 
-                <div className="input-wrapper">
+                <div className='input-wrapper'>
                     <input
                         required={true}
-                        type="email"
-                        id="email"
-                        name="email"
+                        type='email'
+                        id='email'
+                        name='email'
                         value={values.email}
-                        onChange={(e) => {
-                            validateEmail(e);
-                            handleChange(e);
-                        }}
-                    />
-                    <label htmlFor="email">
-                        <span>{t('register.email')}</span>
-                    </label>
-                </div>
-
-                <div className="input-wrapper">
-                    <input
-                        required={true}
-                        type="text"
-                        id="username"
-                        name="username"
-                        value={values.username}
+                        minLength='4'
+                        maxLength='254'
                         onChange={handleChange}
                     />
-                    <label htmlFor="username">
+                    <label htmlFor='email'>
+                        <span>{t('register.email')}</span>
+                    </label>
+                    <AiFillInfoCircle id='emailFillInfoCircle' />
+                    <UncontrolledTooltip
+                        placement='left'
+                        target='emailFillInfoCircle'
+                        className='tooltip'
+                    >
+                        <ul>
+                            <li>min: 4</li> <li>max: 254</li>
+                        </ul>
+                    </UncontrolledTooltip>
+                </div>
+
+                <div className='input-wrapper'>
+                    <input
+                        required={true}
+                        type='text'
+                        id='username'
+                        name='username'
+                        value={values.username}
+                        minLength='5'
+                        maxLength='50'
+                        onChange={handleChange}
+                    />
+                    <label htmlFor='username'>
                         <span>{t('register.username')}</span>
                     </label>
+                    <AiFillInfoCircle id='UsernameFillInfoCircle' />
+                    <UncontrolledTooltip
+                        placement='left'
+                        target='UsernameFillInfoCircle'
+                        className='tooltip'
+                    >
+                        <ul>
+                            <li>min: 5</li> <li>max: 50</li>
+                        </ul>
+                    </UncontrolledTooltip>
                 </div>
 
-                <div className="input-wrapper">
+                <div className='input-wrapper'>
                     <input
                         required={true}
-                        type="password"
-                        id="password"
-                        name="password"
+                        type='password'
+                        id='password'
+                        name='password'
                         value={values.password}
-                        onChange={(e) => {
-                            validatePassword(e);
-                            handleChange(e);
-                        }}
+                        minLength='8'
+                        maxLength='250'
+                        onChange={handleChange}
                     />
-                    <label htmlFor="password">
+                    <label htmlFor='password'>
                         <span>{t('register.password')}</span>
                     </label>
+                    <AiFillInfoCircle id='passwordFillInfoCircle' />
+                    <UncontrolledTooltip
+                        placement='left'
+                        target='passwordFillInfoCircle'
+                        className='tooltip'
+                    >
+                        <ul>
+                            <li>min: 8</li> <li>max: 250</li>
+                        </ul>
+                    </UncontrolledTooltip>
                 </div>
 
-                <div className="input-wrapper">
+                <div className='input-wrapper'>
                     <input
+                        className={`${
+                            !isConfirmPasswordValidate ? 'notMatched' : ''
+                        }`}
                         required={true}
-                        type="password"
-                        id="confirmPassword"
-                        name="confirmPassword"
+                        type='password'
+                        id='confirmPassword'
+                        name='confirmPassword'
                         value={values.confirmPassword}
+                        minLength='8'
+                        maxLength='250'
                         onChange={(e) => {
                             validateConfirmPassword(e);
                             handleChange(e);
                         }}
                     />
-                    <label htmlFor="confirmPassword">
+                    <label htmlFor='confirmPassword'>
                         <span>{t('register.confirmPassword')}</span>
                     </label>
+
+                    {!isConfirmPasswordValidate && (
+                        <>
+                            <AiFillInfoCircle id='confirmPasswordFillInfoCircle' />
+                            <UncontrolledTooltip
+                                placement='left'
+                                target='confirmPasswordFillInfoCircle'
+                                className='tooltip'
+                            >
+                                <ul>
+                                    <li>Passwords do not match</li>
+                                </ul>
+                            </UncontrolledTooltip>
+                        </>
+                    )}
                 </div>
 
                 <button
-                    className="register__button primary-button"
-                    type="submit"
+                    className='register__button primary-button'
+                    type='submit'
                 >
                     {t('register.title')}
                 </button>
-                <NavLink to="sign-in" className="register__link">
+                <NavLink to='sign-in' className='register__link'>
                     {t('register.link')}
                 </NavLink>
             </div>
